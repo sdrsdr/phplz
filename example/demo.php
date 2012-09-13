0dac3f9ea14f06a740888d460d92eecb757a3610
Stoian Ivanov <sdr@mail.bg>
2012-09-14 00:04:31 +0300

    db localization + more comments = better :)

--- example/demo.php
+++ example/demo.php
@@ -1,10 +1,39 @@
 <?php
 require_once ('localize.php');
-//lz_init($_REQUEST['lang']);
+
+/*
+lz_init($lang) will load localization data from "localize.$lang.php" file
+maintained with localize.php tool. To create new localize.$lang.php just 
+edit and copy/rename localize.proto.php from phplz distribution
+*/
 lz_init ('demo');
+
+/*
+now you can use lz function to make translations and mark strings for translation
+which localize.php will update to localize.*.php files.
+*/
 echo "this is a ".lz("test")."!\n";
 $a=array ("some data","more data");//lz("some data") this forces string for translation
 foreach ($a as $d) echo "data: ".lz($d)."\n";
 echo str_replace(array("%data%","%result%"),array("important","creative"),lz("it is %data% to be %result%"))."\n";
 // lz("for translation");
+
+/*db result localization:
+to localize database data you need to have a localization column for every column you would like to localize 
+that must have the same name as the one for localization but with suffix _lz. So to localize column 'prodname' you need
+column 'prodname_lz' containing serialized assoc aray with lzdata for evey loacalization you need:
+*/
+
+	$a=array (
+		'prodname'=>'translate me',
+		'prodname_lz'=>serialize(array(
+			'demo'=>array('translate me'=>'foo'),
+			'bg'=>array('translate me'=>'bar'),
+		)),
+	);
+	
+	lzaa ($a);
+	
+	echo "Localized db string: ".$a['prodname']."\n";
+	
 ?>
--- localize.php
+++ localize.php
@@ -1,15 +1,50 @@
 <?php
 function lz_init($lang){
 	global $lz_data;
+	global $lz_init_lang;
+	$lz_init_lang=$lang;
 	$lz_data=NULL;
 	if (file_exists("localize.$lang.php")) {
 		require_once("localize.$lang.php");
 	}
 }
+/*
+global $lz_data;
+$lz_data=array('translate me'=>'foo','translate me too'=>'bar');
 
+lz('translate me') returns 'foo'
+lz('translate me too') returns 'bar'
+*/
 function lz($s){
 	global $lz_data;
 	if (is_array($lz_data) && array_key_exists($s,$lz_data)) return $lz_data[$s];
 	return $s;
+}
+
+/*
+	global $lz_init_lang;$lz_init_lang='en';
+	$a=array (
+		'test'=>'translate me'
+		'test_lz'=>serialize(array(
+			'en'=>array('translate me'=>'foo'),
+			'bg'=>array('translate me'=>'bar'),
+		))
+	);
+	
+	$lzaa($a);
+	
+	$a['test'] will be set to 'foo'
+*/
+function lzaa(&$a){
+	global  $lz_init_lang;
+	foreach ($a as $k=>$v) {
+		if (array_key_exists($k."_lz",$a)) {
+			$lz_data=$a[$k."_lz"];
+			if (!is_array($lz_data)) $lz_data=@unserialize($lz_data);
+			if (is_array($lz_data) && array_key_exists($lz_init_lang,$lz_data)) {
+				if (is_array($lz_data[$lz_init_lang]) && array_key_exists($a[$k],$lz_data[$lz_init_lang])) $a[$k]=$lz_data[$lz_init_lang][$a[$k]];
+			}
+		}
+	}
 }
 ?>
